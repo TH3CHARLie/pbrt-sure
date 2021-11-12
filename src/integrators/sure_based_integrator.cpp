0cbda3308bb202acd1eef8bc241e4405fcc52528
Xuanda Yang <th3charlie@gmail.com>
2021-11-11 23:36:57 -0800

    format and add some args

--- src/integrators/sure_based_integrator.cpp
+++ src/integrators/sure_based_integrator.cpp
@@ -1,4 +1,5 @@
 #include "integrators/sure_based_integrator.h"
+
 #include "lightdistrib.h"
 #include "progressreporter.h"
 
@@ -16,7 +17,7 @@ SUREBasedIntegrator *CreateSUREBasedIntegrator(
                   np);
         else {
             pixel_bounds = Intersect(pixel_bounds,
-                                    Bounds2i{{pb[0], pb[2]}, {pb[1], pb[3]}});
+                                     Bounds2i{{pb[0], pb[2]}, {pb[1], pb[3]}});
             if (pixel_bounds.Area() == 0)
                 Error("Degenerate \"pixelbounds\" specified.");
         }
@@ -24,12 +25,18 @@ SUREBasedIntegrator *CreateSUREBasedIntegrator(
     Float rr_threshold = params.FindOneFloat("rrthreshold", 1.);
     std::string light_strategy =
         params.FindOneString("lightsamplestrategy", "spatial");
-    PathIntegrator *path_integrator = new PathIntegrator(max_depth, camera, sampler, pixel_bounds,
-                              rr_threshold, light_strategy);
-    return new SUREBasedIntegrator(sampler, camera, std::shared_ptr<PathIntegrator>(path_integrator), pixel_bounds);
+    PathIntegrator *path_integrator = new PathIntegrator(
+        max_depth, camera, sampler, pixel_bounds, rr_threshold, light_strategy);
+
+    int64_t sample_budget = params.FindOneInt("samplebudget", 64);
+    int64_t num_initial_samples = params.FindOneInt("initialsamples", 8);
+    int64_t num_adaptive_samples = params.FindOneInt("adaptivesamples", 8);
+    return new SUREBasedIntegrator(
+        sampler, camera, std::shared_ptr<PathIntegrator>(path_integrator),
+        pixel_bounds, sample_budget, num_initial_samples, num_adaptive_samples);
 }
 
-void SUREBasedIntegrator::Render(const Scene& scene) {
+void SUREBasedIntegrator::Render(const Scene &scene) {
     path_integrator->Preprocess(scene, *sampler);
     Bounds2i sample_bounds = camera->film->GetSampleBounds();
     Vector2i sample_extent = sample_bounds.Diagonal();
@@ -37,70 +44,63 @@ void SUREBasedIntegrator::Render(const Scene& scene) {
     Point2i num_tiles((sample_extent.x + tile_size - 1) / tile_size,
                       (sample_extent.y + tile_size - 1) / tile_size);
 
-    ProgressReporter reporter(num_tiles.x * num_tiles.y, "SURE-Based Integrator rendering");
+    ProgressReporter reporter(num_tiles.x * num_tiles.y,
+                              "SURE-Based Integrator rendering");
     {
-        ParallelFor2D([&](Point2i tile) {
-            MemoryArena arena;
+        ParallelFor2D(
+            [&](Point2i tile) {
+                MemoryArena arena;
 
-            int seed = tile.y * num_tiles.x + tile.x;
-            std::unique_ptr<Sampler> tile_sampler = sampler->Clone(seed);
-            int x0 = sample_bounds.pMin.x + tile.x * tile_size;
-            int x1 = std::min(x0 + tile_size, sample_bounds.pMax.x);
-            int y0 = sample_bounds.pMin.y + tile.y * tile_size;
-            int y1 = std::min(y0 + tile_size, sample_bounds.pMax.y);
-            Bounds2i tile_bounds(Point2i(x0, y0), Point2i(x1, y1));
+                int seed = tile.y * num_tiles.x + tile.x;
+                std::unique_ptr<Sampler> tile_sampler = sampler->Clone(seed);
+                int x0 = sample_bounds.pMin.x + tile.x * tile_size;
+                int x1 = std::min(x0 + tile_size, sample_bounds.pMax.x);
+                int y0 = sample_bounds.pMin.y + tile.y * tile_size;
+                int y1 = std::min(y0 + tile_size, sample_bounds.pMax.y);
+                Bounds2i tile_bounds(Point2i(x0, y0), Point2i(x1, y1));
 
-            std::unique_ptr<FilmTile> film_tile = camera->film->GetFilmTile(tile_bounds);
-            for (Point2i pixel: tile_bounds) {
-                tile_sampler->StartPixel(pixel);
+                std::unique_ptr<FilmTile> film_tile =
+                    camera->film->GetFilmTile(tile_bounds);
+                for (Point2i pixel : tile_bounds) {
+                    tile_sampler->StartPixel(pixel);
 
-                if (!InsideExclusive(pixel, pixel_bounds)) {
-                    continue;
+                    if (!InsideExclusive(pixel, pixel_bounds)) {
+                        continue;
+                    }
+
+                    do {
+                        CameraSample camera_sample =
+                            tile_sampler->GetCameraSample(pixel);
+
+                        RayDifferential ray;
+                        Float ray_weight = camera->GenerateRayDifferential(
+                            camera_sample, &ray);
+                        ray.ScaleDifferentials(
+                            1 /
+                            std::sqrt((Float)tile_sampler->samplesPerPixel));
+                        Spectrum L(0.f);
+                        if (ray_weight > 0) {
+                            L = path_integrator->Li(ray, scene, *tile_sampler,
+                                                    arena, 0);
+                        }
+
+                        if (L.HasNaNs()) {
+                            L = Spectrum(0.f);
+                        } else if (L.y() < -1e-5) {
+                            L = Spectrum(0.f);
+                        } else if (std::isinf(L.y())) {
+                            L = Spectrum(0.f);
+                        }
+                        film_tile->AddSample(camera_sample.pFilm, L,
+                                             ray_weight);
+
+                        arena.Reset();
+                    } while (tile_sampler->StartNextSample());
                 }
-
-                do {
-                    CameraSample camera_sample = tile_sampler->GetCameraSample(pixel);
-
-                    RayDifferential ray;
-                    Float ray_weight =
-                        camera->GenerateRayDifferential(camera_sample, &ray);
-                    ray.ScaleDifferentials(
-                        1 / std::sqrt((Float)tile_sampler->samplesPerPixel));
-                    Spectrum L(0.f);
-                    if (ray_weight > 0) {
-                        L = path_integrator->Li(ray, scene, *tile_sampler, arena, 0);
-                    }
-
-                    if (L.HasNaNs()) {
-                        LOG(ERROR) << StringPrintf(
-                            "Not-a-number radiance value returned "
-                            "for pixel (%d, %d), sample %d. Setting to black.",
-                            pixel.x, pixel.y,
-                            (int)tile_sampler->CurrentSampleNumber());
-                        L = Spectrum(0.f);
-                    } else if (L.y() < -1e-5) {
-                        LOG(ERROR) << StringPrintf(
-                            "Negative luminance value, %f, returned "
-                            "for pixel (%d, %d), sample %d. Setting to black.",
-                            L.y(), pixel.x, pixel.y,
-                            (int)tile_sampler->CurrentSampleNumber());
-                        L = Spectrum(0.f);
-                    } else if (std::isinf(L.y())) {
-                          LOG(ERROR) << StringPrintf(
-                            "Infinite luminance value returned "
-                            "for pixel (%d, %d), sample %d. Setting to black.",
-                            pixel.x, pixel.y,
-                            (int)tile_sampler->CurrentSampleNumber());
-                        L = Spectrum(0.f);
-                    }
-                    film_tile->AddSample(camera_sample.pFilm, L, ray_weight);
-
-                    arena.Reset();
-                } while (tile_sampler->StartNextSample());
-            }
-            camera->film->MergeFilmTile(std::move(film_tile));
-            reporter.Update();
-        }, num_tiles);
+                camera->film->MergeFilmTile(std::move(film_tile));
+                reporter.Update();
+            },
+            num_tiles);
         reporter.Done();
     }
 
--- src/integrators/sure_based_integrator.h
+++ src/integrators/sure_based_integrator.h
@@ -1,41 +1,51 @@
 #ifndef SURE_BASED_INTEGRATOR_H
 #define SURE_BASED_INTEGRATOR_H
 
+#include <cstdint>
+#include <memory>
+
 #include "camera.h"
 #include "integrator.h"
 #include "integrators/path.h"
-#include "pbrt.h"
 #include "paramset.h"
+#include "pbrt.h"
 #include "sampler.h"
-#include <memory>
 
 namespace pbrt {
-class AdaptiveSampler : public Sampler {
-
-};
 
 class SUREBasedIntegrator : public Integrator {
   public:
     SUREBasedIntegrator(std::shared_ptr<Sampler> sampler,
                         std::shared_ptr<const Camera> camera,
-                        std::shared_ptr<PathIntegrator> path_integrator,\
-                        const Bounds2i pixel_bounds)
-        : sampler(sampler), camera(camera), path_integrator(path_integrator), pixel_bounds(pixel_bounds) {}
+                        std::shared_ptr<PathIntegrator> path_integrator,
+                        const Bounds2i pixel_bounds,
+                        const int64_t sample_budget,
+                        const int64_t num_initial_samples,
+                        const int64_t num_adaptive_samples)
+        : sampler(sampler),
+          camera(camera),
+          path_integrator(path_integrator),
+          pixel_bounds(pixel_bounds),
+          sample_budget(sample_budget),
+          num_initial_samples(num_initial_samples),
+          num_adaptive_samples(num_adaptive_samples) {}
 
     virtual ~SUREBasedIntegrator() {}
 
-    virtual void Render(const Scene& scene);
+    virtual void Render(const Scene &scene);
 
   private:
     std::shared_ptr<Sampler> sampler;
     std::shared_ptr<const Camera> camera;
     std::shared_ptr<PathIntegrator> path_integrator;
     const Bounds2i pixel_bounds;
+    const int64_t sample_budget;
+    const int64_t num_initial_samples;
+    const int64_t num_adaptive_samples;
 };
 
 SUREBasedIntegrator *CreateSUREBasedIntegrator(
-    const ParamSet &params,
-    std::shared_ptr<Sampler> sampler,
+    const ParamSet &params, std::shared_ptr<Sampler> sampler,
     std::shared_ptr<const Camera> camera);
 }  // namespace pbrt
 
